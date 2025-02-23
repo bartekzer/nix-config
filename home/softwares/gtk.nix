b29043063fbdcbcba16234306a5e25f55a98a36a
Lokasku <lukasku@proton.me>
2025-02-23 10:32:07 +0100

    Update configuration

--- home/softwares/gtk.nix
+++ home/softwares/gtk.nix
@@ -7,12 +7,13 @@
     };
     iconTheme = {
       package = pkgs.whitesur-icon-theme.overrideAttrs (oldAttrs: {
+        /*
+        rm $out/share/icons/WhiteSur-dark/actions/32/folder.svg
+        rm $out/share/icons/WhiteSur/actions/32/folder.svg
+        */
         preFixup =
           (oldAttrs.preFixup or "")
           + ''
-            rm $out/share/icons/WhiteSur-dark/actions/32/folder.svg
-            rm $out/share/icons/WhiteSur/actions/32/folder.svg
-
             rm $out/share/icons/WhiteSur-light/status/22/notification-display-brightness.svg
             rm $out/share/icons/WhiteSur/status/22/notification-display-brightness.svg
           '';
